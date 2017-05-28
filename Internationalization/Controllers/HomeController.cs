06fe27a5c1313eef3f015ff8d433bcdcdf3801d4
Jimmy McWhinney <jimmy.mcw@live.com>
2017-05-29 05:19:06 +0800

    Initial commit.

--- Internationalization/Controllers/HomeController.cs
+++ Internationalization/Controllers/HomeController.cs
@@ -10,6 +10,8 @@ namespace Internationalization.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["Message"] = "The home page.";
+
             return View();
         }
 
